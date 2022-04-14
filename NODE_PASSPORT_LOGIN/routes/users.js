208a1005075f589cdba1ff8f98b9e7b9fd15e54c
Raees <f190963@nu.edu.pk>
2022-04-14 20:47:07 +0500

    Error Handling

--- NODE_PASSPORT_LOGIN/routes/users.js
+++ NODE_PASSPORT_LOGIN/routes/users.js
@@ -8,4 +8,32 @@ router.get('/login', (req, res) => res.render('login'));
 // Register Page
 router.get('/register', (req, res) => res.render('register'));
 
+//Rsgister handle
+router.post('/register', (req,res) => {
+    const {name, email, password, password2 } = req.body;
+    let errors = [];
+// Check required fields
+if(!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please fill in all fields' });
+}
+
+// Check Passwords match
+if(password !== password2) {
+    errors.push({ msg: 'Password do not match' });
+}
+
+// Check pass length
+if(errors.length > 0) {
+res.render('register', {
+errors,
+name,
+email,
+password,
+password2
+});
+} else {
+    res.send('pass')
+}
+});
+
 module.exports = router;
