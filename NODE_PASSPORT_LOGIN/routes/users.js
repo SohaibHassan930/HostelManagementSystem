8cee4dafe20d59534dab615bde370ce0f2cce5b5
Raees <f190963@nu.edu.pk>
2022-04-21 01:44:11 +0500

    UI Design

--- NODE_PASSPORT_LOGIN/routes/users.js
+++ NODE_PASSPORT_LOGIN/routes/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+//user model
+const User = require('../models/User')
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -32,8 +36,58 @@ password,
 password2
 });
 } else {
-    res.send('pass')
+User.findOne({ email: email })
+.then(user => {
+    if(user) {
+        // User exists
+        errors.push({ msg: 'Email is already registered '});
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        const newUser = new User({
+            name,
+            email,
+            password
+        });
+
+        // Hash Password
+        bcrypt.genSalt(10, (err, salt) =>
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            // set password to hashed
+            newUser.password = hash;
+            //Save user
+            newUser.save()
+            .then(user => {
+                req.flash('succes_msg', 'You are now registered and can log in');
+                res.redirect('users/login');
+            })
+            .catch(err => console.log(err));
+        }))
+    }
+});
 }
 });
+
+//Login Handle
+router.post('/login', (req, res, next) => {
+passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/users/login',
+    failureFlash: true
+})(req, res, next);
+});
+
+// Logout Handle
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+})
 
 module.exports = router;
